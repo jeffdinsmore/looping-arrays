955f1ce5a5d82c25e0fc12b716bc4a3c0cf7567c
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-21 14:58:52 -0700

    still researching on how to figure out first test that would compare two arrays and push an output to new array

--- js/scripts.js
+++ js/scripts.js
@@ -13,31 +13,45 @@
 
 $(document).ready(function() {
   $("form#go").submit(function(event) {
-  
+    const numbers3 = [3,13];
     const userInput = parseInt($("#input").val());
     let numbers = Array.from(Array(userInput + 1).keys());
     let number3 = numbers.filter(input => input.toString().includes("3"));
     
     console.log(number3);
     
-    
-    let newArray = [];
-      for (let i = 0; i < numbers.length; i++) {
-        for (let j = 0; j < number3.length; j ++) 
-        if (numbers.includes("3")) {
-          newArray.push(" Won't you be my neighbor?");
-        // } else if (numbers[i] === 2) {
-        //   newArray.push(" Boop!")
-        // } else if (numbers[i] === 1) {
-        //   newArray.push(" Beep!")
-        }
-        else if (numbers[i] !== number3[j]) {
-          newArray.push(" " + i);
-      
-      
-      
-      }
+
+  
+// const stringArray = numbers3.split(" ");
+let newArray = [];
+stringArray.forEach(function(number) {
+    if (number3.includes(number)) {
+        newArray.push("goober");
+    } else {
+      newArray.push(number);
     }
+});
+
+const finalArray = newArray.join(" ");
+
+    
+    // let newArray = [];
+    //   for (let i = 0; i < numbers.length; i++) {
+    //     for (let j = 0; j < number3.length; j ++) 
+    //     if (numbers.includes("3")) {
+    //       newArray.push(" Won't you be my neighbor?");
+    //     // } else if (numbers[i] === 2) {
+    //     //   newArray.push(" Boop!")
+    //     // } else if (numbers[i] === 1) {
+    //     //   newArray.push(" Beep!")
+    //     }
+    //     else if (numbers[i] !== number3[j]) {
+    //       newArray.push(" " + i);
+      
+      
+      
+    //   }
+    // }
     
       // if (element === 3) {
       // newArray.push(" Won't you be my neighbor?");
@@ -51,7 +65,7 @@ $(document).ready(function() {
   
     
     
-    $("#result").text(newArray);
+    $("#result").text(finalArray);
     event.preventDefault();
   });
 });
