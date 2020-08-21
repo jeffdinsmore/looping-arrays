8406ecdb2c96febc5ca74144c22ea204f1924747
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-21 13:39:52 -0700

    added an extra step into javascript to compare the two arrays

--- js/scripts.js
+++ js/scripts.js
@@ -21,24 +21,24 @@ $(document).ready(function() {
     
     
     let newArray = [];
-    numbers.forEach(function(element1, element2) {
-      for (let i = 0; i < numbers.length; i++) {
-        for (let j = 0; j < number3.length; j ++)
-        if (element1[i] === element2[j]) {
+      for (let i = 0; i < number3.length; i++) {
+        for (let j = 0; j < numbers.length; j ++)
+        if (numbers[j] === number3[i]) {
           newArray.push(" Won't you be my neighbor?");
-        } else if (element1 === 2) {
-          newArray.push(" Boop!")
-        } else if (element1 === 1) {
-          newArray.push(" Beep!")
+        // } else if (numbers[i] === 2) {
+        //   newArray.push(" Boop!")
+        // } else if (numbers[i] === 1) {
+        //   newArray.push(" Beep!")
         } else {
-        newArray.push(" " + element1);
+        newArray.push(" " + i);
+      
       }
-      }
+    }
     
       // if (element === 3) {
       // newArray.push(" Won't you be my neighbor?");
       // } 
-    });
+  
     // console.log(newArray);
 
     // console.log(numbers);
