a3e784aee7ddf38e3beda5716863724cc75ab7ee
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-21 11:04:53 -0700

    testing loop to pull out instances of 3

--- js/scripts.js
+++ js/scripts.js
@@ -18,19 +18,19 @@ $(document).ready(function() {
     let numbers = Array.from(Array(userInput + 1).keys());
     
     
-    let doubledArray = [];
+    let newArray = [];
     numbers.forEach(function(element) {
       if (element === 3) {
-      doubledArray.push(" Won't you be my neighbor?");
+      newArray.push(" Won't you be my neighbor?");
       } else if (element === 2) {
-          doubledArray.push(" Boop!")
+          newArray.push(" Boop!")
       } else if (element === 1) {
-          doubledArray.push(" Beep!")
+          newArray.push(" Beep!")
       } else {
-        doubledArray.push(" " + element);
+        newArray.push(" " + element);
       }
     });
-    console.log(doubledArray);
+    console.log(newArray);
 
     console.log(numbers);
 
@@ -38,7 +38,7 @@ $(document).ready(function() {
   
     
     
-    $("#result").text(doubledArray);
+    $("#result").text(newArray);
     event.preventDefault();
   });
 });
