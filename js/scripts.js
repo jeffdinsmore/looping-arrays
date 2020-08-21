2173722d60971a595b32bc77a3445a6ebde990cd
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-21 16:11:59 -0700

    test two tested and complete

--- js/scripts.js
+++ js/scripts.js
@@ -17,11 +17,17 @@ $(document).ready(function() {
     const userInput = parseInt($("#input").val());
     let numbers = Array.from(Array(userInput + 1).keys());
     let number3 = numbers.filter(input => input.toString().includes("3"));
+    let number2 = numbers.filter(input => input.toString().includes("2"));
+    let number2 = numbers.filter(input => input.toString().includes("2"));
     
     let xArray = [];
     numbers.forEach(function(letter) {
       if (number3.includes(letter)) {
         xArray.push("Won't you be my neighbor?");
+      } else if (number2.includes(letter)) {
+        xArray.push("Boop!");
+      } else if (number1.includes(letter)) {
+        xArray.push("Beep!");
       } else {
         xArray.push(letter);
       }
