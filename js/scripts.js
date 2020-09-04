4177c439305bbc8b7fae7e01a58500ffe050907b
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-03 17:32:31 -0700

    removed my business logic from the UI logic and into the business logic section of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,33 @@
 // Business logic:
-
-
-
-
+function replaceNumbers(numberInput) {
+  let numbers = Array.from(Array(numberInput + 1).keys());
+  let number3 = numbers.filter(input => input.toString().includes("3"));
+  let number2 = numbers.filter(input => input.toString().includes("2"));
+  let number1 = numbers.filter(input => input.toString().includes("1"));
+  let newArray = [];
+  numbers.forEach(function(number) {
+    if (number3.includes(number)) {
+      newArray.push("Won't you be my neighbor?");
+    } else if (number2.includes(number)) {
+      newArray.push("Boop!");
+    } else if (number1.includes(number)) {
+      newArray.push("Beep!");
+    } else {
+      newArray.push(number);
+    }
+  });
+  return newArray;
+}
 
 // User interface logic:
 
 $(document).ready(function() {
   $("form#go").submit(function(event) {
-    
+    event.preventDefault();
     const userInput = parseInt($("#input").val());
-    let numbers = Array.from(Array(userInput + 1).keys());
-    console.log(numbers);
-    let number3 = numbers.filter(input => input.toString().includes("3"));
-    let number2 = numbers.filter(input => input.toString().includes("2"));
-    let number1 = numbers.filter(input => input.toString().includes("1"));
-    
-    let newArray = [];
-    numbers.forEach(function(number) {
-      if (number3.includes(number)) {
-        newArray.push("Won't you be my neighbor?");
-      } else if (number2.includes(number)) {
-        newArray.push("Boop!");
-      } else if (number1.includes(number)) {
-        newArray.push("Beep!");
-      } else {
-        newArray.push(number);
-      }
-    });
-    const finalString = newArray.join(", ");
-
+    newArray2 = replaceNumbers(userInput);
+    const finalString = newArray2.join(", ");
     $(".pink").show();
     $("#result").text(finalString);
-    event.preventDefault();
   });
 });
